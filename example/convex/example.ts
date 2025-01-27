34ed9d0bc1afb225ad795284a2755ab4c6576cfd
Lee Danilek <lee@convex.dev>
2025-01-27 13:27:10 -0500

    .

--- example/convex/example.ts
+++ example/convex/example.ts
@@ -13,7 +13,7 @@ const highPriPool = new Workpool(components.highPriWorkpool, {
   maxParallelism: 20,
   // For tests, disable completed work cleanup.
   statusTtl: Number.POSITIVE_INFINITY,
-  logLevel: "DEBUG",
+  logLevel: "INFO",
 });
 const pool = new Workpool(components.workpool, {
   maxParallelism: 3,
