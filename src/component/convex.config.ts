fb078ab4e8efec6c5e2e7740851410aa6dafb0d7
Ian Macartney <ianhmacartney@gmail.com>
2025-02-20 19:38:34 -0800

    snip crons

--- src/component/convex.config.ts
+++ src/component/convex.config.ts
@@ -1,8 +1,5 @@
 import { defineComponent } from "convex/server";
-import crons from "@convex-dev/crons/convex.config";
 
 const component = defineComponent("workpool");
-
-component.use(crons);
 
 export default component;
