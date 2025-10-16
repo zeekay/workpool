ed50c4c8c9dddbfb40fe3b47302eaea5391d0fdc
Ian Macartney <ianhmacartney@gmail.com>
2025-10-16 00:17:34 -0700

    ui env comes from root

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  envDir: "../",
   plugins: [react()],
   resolve: {
     conditions: ["@convex-dev/component-source"],
