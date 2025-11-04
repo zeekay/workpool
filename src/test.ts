40c103d1af3f36a323eadb5f7ec4beca5ed9dc45
Ian Macartney <ianhmacartney@gmail.com>
2025-11-04 00:52:57 -0800

    improve configs

--- src/test.ts
+++ src/test.ts
@@ -2,10 +2,16 @@ import type { TestConvex } from "convex-test";
 import type { GenericSchema, SchemaDefinition } from "convex/server";
 import schema from "./component/schema.js";
 const modules = import.meta.glob("./component/**/*.ts");
-function register(
+
+/**
+ * Register the component with the test convex instance.
+ * @param t - The test convex instance, e.g. from calling `convexTest`.
+ * @param name - The name of the component, as registered in convex.config.ts.
+ */
+export function register(
   t: TestConvex<SchemaDefinition<GenericSchema, boolean>>,
   name: string = "workpool"
 ) {
   t.registerComponent(name, schema, modules);
 }
-export default { schema, modules, register };
+export default { register, schema, modules };
