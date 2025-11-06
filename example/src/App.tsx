4244abb2e41df681858db9638cc7ec8ce02351b3
Ian Macartney <ianhmacartney@gmail.com>
2025-11-06 00:24:29 -0800

    typecheck

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,16 +1,10 @@
 import "./App.css";
-import { useMutation, useQuery } from "convex/react";
-import { api } from "../convex/_generated/api";
 
 function App() {
-  const count = useQuery(api.app.getCount);
-  const addOne = useMutation(api.app.addOne);
-
   return (
     <>
-      <h1>Convex Counter Component Example</h1>
+      <h1>Convex Workpool Component Example</h1>
       <div className="card">
-        <button onClick={() => addOne()}>count is {count}</button>
         <p>
           See <code>example/convex/example.ts</code> for all the ways to use
           this component
